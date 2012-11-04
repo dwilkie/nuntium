ae493d0d16fbbd8dab357a07c144e7a07b30f54c
David Wilkie <dwilkie@gmail.com>
2012-11-04 19:55:19 +0700

    Refactor monit rake task and test it using Rspec

---
+++ spec/models/monit_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+
+describe Monit do
+  include FakeFS::SpecHelpers
+
+  CONFIG_FILE_PATH = "#{Rails.root}/config/monit_services.yml".freeze
+
+  let(:default_nuntium_services) {[
+    "cron",
+    "managed_processes",
+    {"generic_worker" => {"slow" => 1, "fast" => 1}},
+    "scheduled_jobs_service"
+  ]}
+
+  let(:default_output_path) { "#{Rails.root}/nuntium" }
+
+  def monit_output(options = {})
+    options[:path] ||= default_output_path
+    File.open(options[:path], 'r') { |file| file.read }
+  end
+
+  def generate_nuntium_monit_config_file!(options = {})
+    options[:services] ||= default_nuntium_services
+    FileUtils.mkdir_p(File.dirname(CONFIG_FILE_PATH))
+    File.open(CONFIG_FILE_PATH, 'w') { |file| file.write(options[:services].to_yaml) }
+  end
+
+  def assert_monit_script(output, options = {})
+    options[:services] ||= default_nuntium_services
+    options[:services].each do |service|
+      if service.is_a?(Hash)
+        service_name = service.keys.first
+        service.values.first.each do |type, worker_id|
+          assert_pidfile(output, service_name, type, worker_id)
+          assert_operate_service(output, service_name, "start", type, worker_id)
+          assert_operate_service(output, service_name, "stop", type, worker_id)
+        end
+      else
+        service_name = service
+        assert_pidfile(output, service_name)
+        assert_operate_service(output, service_name, "start")
+        assert_operate_service(output, service_name, "stop")
+      end
+
+      output.should include("check process nuntium_#{service_name}")
+      output.should include("group nuntium")
+    end
+  end
+
+  def assert_operate_service(output, service_name, action, type = nil, worker_id = nil)
+    operation_string = %{#{action} "/bin/su - #{ENV['USER']} -c '#{Rails.root}/script/nuntium_service.sh #{service_name}_daemon_ctl.rb #{action} #{Rails.env}}
+    operation_string << " #{type}" if type
+    operation_string << " #{worker_id}" if worker_id
+    operation_string << %{'"}
+    output.should include(operation_string)
+  end
+
+  def assert_pidfile(output, service_name, type = nil, worker_id = nil)
+    pidfile_string = "with pidfile #{Rails.root}/tmp/pids/#{service_name}_daemon"
+    pidfile_string << ".#{type}" if type
+    pidfile_string << ".#{worker_id}." if worker_id
+    pidfile_string << ".pid"
+    output.should include(pidfile_string)
+  end
+
+  describe ".generate_config!" do
+    context "given there's a nuntium monit services config file 'config/monit_services.yml'" do
+      before do
+        generate_nuntium_monit_config_file!
+      end
+
+      context "passing no options" do
+        it "create a monit script under the Rails root directory" do
+          subject.class.generate_config!.should == default_output_path
+          assert_monit_script(monit_output)
+        end
+      end
+
+      context "passing :path => '/home/monit_script'" do
+        it "should create a monit script under '/home/monit_script'" do
+          subject.class.generate_config!(:path => "/home/monit_script").should == "/home/monit_script"
+          assert_monit_script(monit_output(:path => "/home/monit_script"))
+        end
+      end
+    end
+  end
+end
