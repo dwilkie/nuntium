95bdbfc7e4055e340bda18a60609b36f42cb7806
David Wilkie <dwilkie@gmail.com>
2012-11-08 14:01:02 +0700

    parse correct argument as rabbitmqctl_path

--- script/check_overloaded_queues.rb
+++ script/check_overloaded_queues.rb
@@ -2,4 +2,6 @@
 
 require File.expand_path("../../app/models/monit", __FILE__)
 
-raise "Nuntium has overloaded queues!" if Monit.overloaded_queues(:environment => ARGV[0], :rabbitmqctl_path => ARGV[0]).any?
+raise "Nuntium has overloaded queues!" if Monit.overloaded_queues(
+  :environment => ARGV[0], :rabbitmqctl_path => ARGV[1]
+).any?
