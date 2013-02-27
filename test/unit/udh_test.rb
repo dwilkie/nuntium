8b8045ff25e568138aee836106dc427ddabf6014
David Wilkie <dwilkie@gmail.com>
2013-02-27 16:41:27 +0700

    Merge with remote at https://bitbucket.org/instedd/nuntium

--- test/unit/udh_test.rb
+++ test/unit/udh_test.rb
@@ -29,7 +29,7 @@ class UdhTest < ActiveSupport::TestCase
   end
 
   test "from simple" do
-    udh = Udh.new("\x05\x00\x03\xf5\x12\x02")
+    udh = Udh.new([5, 0, 3, 0xF5, 0x12, 0x02])
     assert_equal 5, udh.length
     assert_equal 0xF5, udh[0][:reference_number]
     assert_equal 0x12, udh[0][:part_count]
@@ -37,21 +37,16 @@ class UdhTest < ActiveSupport::TestCase
   end
 
   test "ignore other headers" do
-    udh = Udh.new("\x06\x05\x04\xc3\x50\x00\x00")
+    udh = Udh.new([6, 5, 4, 195, 80, 0, 0])
     assert_equal 6, udh.length
     assert_nil udh[0]
   end
 
   test "from complex" do
-    udh = Udh.new("\x0b\x08\x04\xc3\x50\x00\x00\x00\x03\xf5\x12\x02")
+    udh = Udh.new([11, 8, 4, 195, 80, 0, 0, 0, 3, 245, 18, 2])
     assert_equal 11, udh.length
     assert_equal 0xF5, udh[0][:reference_number]
     assert_equal 0x12, udh[0][:part_count]
     assert_equal 0x02, udh[0][:part_number]
-  end
-
-  test "skip" do
-    udh = Udh.new("\x0b\x08\x04\xc3\x50\x00\x00\x00\x03\xf5\x12\x02hello")
-    assert_equal 'hello', udh.skip("\x0b\x08\x04\xc3\x50\x00\x00\x00\x03\xf5\x12\x02hello")
   end
 end
