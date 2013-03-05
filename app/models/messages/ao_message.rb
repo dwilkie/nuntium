f0959214b5a7bfdf55dfb2169b8b704887494952
David Wilkie <dwilkie@gmail.com>
2013-03-05 11:12:24 +0700

    Revert "Log what's happening when delivery receipts are not being delivered"
    This reverts commit 03eb95dafd88603f0581299766f096c0f94f2003.

--- app/models/messages/ao_message.rb
+++ app/models/messages/ao_message.rb
@@ -1,20 +1,3 @@
-# Copyright (C) 2009-2012, InSTEDD
-#
-# This file is part of Nuntium.
-#
-# Nuntium is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Nuntium is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Nuntium.  If not, see <http://www.gnu.org/licenses/>.
-
 class AoMessage < ActiveRecord::Base
   belongs_to :account
   belongs_to :application
@@ -75,24 +58,6 @@ class AoMessage < ActiveRecord::Base
   private
 
   def send_delivery_ack
-    unless changed?
-      account.logger.warning("Not posting delivery receipt because message was not changed. State is: #{state}")
-    end
-
-    unless state == 'failed' || state == 'delivered' || state == 'confirmed'
-      account.logger.warning("Not posting delivery receipt because message state is not 'failed' || 'delivered' || 'confirmed'. State is: #{state}")
-    end
-
-    unless channel_id
-      account.logger.warning("Not posting delivery receipt because channel_id is nil. State is: #{state}")
-    end
-
-    app = self.application
-
-    unless app.try(:delivery_ack_method) != 'none'
-      account.logger.warning("Not posting delivery receipt because app.delivery_ack_method == 'none' State is: #{state}")
-    end
-
     return unless changed?
 
     return true unless state == 'failed' || state == 'delivered' || state == 'confirmed'
