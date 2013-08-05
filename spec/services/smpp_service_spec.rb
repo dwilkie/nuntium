9ecc7fb7105496e797e76282ec53a6223cdc0eec
David Wilkie <dwilkie@gmail.com>
2013-08-05 12:59:42 +0700

    don't switch to backup on service 8 errors

--- spec/services/smpp_service_spec.rb
+++ spec/services/smpp_service_spec.rb
@@ -12,7 +12,6 @@ describe SmppGateway do
   describe "#message_rejected(transceiver, mt_message_id, pdu)" do
     context "the message fails due to a command status 8" do
       def assert_alert(msg, options = {})
-        options[:channel_alert] = true unless options[:channel_alert] == false
         Rails.logger.should_receive(:warn).with(/#{msg}/)
         if options[:channel_alert]
           channel.should_receive(:alert).with(/#{msg}/)
@@ -25,31 +24,10 @@ describe SmppGateway do
         pdu.stub(:command_status).and_return(8)
       end
 
-      it "should try to switch to the backup channel (if available)" do
-        channel.should_receive(:switch_to_backup)
+      it "should not try to switch to the backup channel" do
+        channel.should_not_receive(:switch_to_backup)
+        assert_alert("Received command status 8")
         subject.message_rejected(transceiver, ao_message.id, pdu)
-      end
-
-      context "and there is a backup channel available" do
-        before do
-          channel.stub(:switch_to_backup).and_return(true)
-        end
-
-        it "should include alert that the channel is switching to backup" do
-          assert_alert("Switching to backup")
-          subject.message_rejected(transceiver, ao_message.id, pdu)
-        end
-      end
-
-      context "and there is no backup channel available" do
-        before do
-          channel.stub(:switch_to_backup).and_return(false)
-        end
-
-        it "should log that no backup channel is available but not send an email" do
-          assert_alert("WARNING: No backup channel available", :channel_alert => false)
-          subject.message_rejected(transceiver, ao_message.id, pdu)
-        end
       end
     end
   end
